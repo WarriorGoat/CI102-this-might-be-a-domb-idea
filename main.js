127baf41096e2f8ce92f8b75713c65d6c53b9e31
WarriorGoat <michael.d.spitz@gmail.com>
2022-10-24 15:28:18 -0400

    Uploaded project files.

---
+++ main.js
@@ -0,0 +1,24 @@
+let paragraph = document.querySelector("p")
+paragraph.style.color = "rgb(124, 210, 231)";
+
+let heading = document.querySelector("h1");
+heading.style.fontSize = ("48px");
+
+paragraph.innerText = "orem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let no13 = document.querySelector("#item-13");
+no13.style.color = ("rgba(0, 0, 0, 0.5)");
+
+let no3 = document.querySelector("#item-3");
+no3.innerText = ("Hi!");
+let myImage = document.querySelector("img");
+myImage.src = "https://di-uploads-pod15.dealerinspire.com/orangecoastchryslerjeepdodgeram/uploads/2020/08/Jeep-Models-Costa-Mesa-CA-1950s-Jeep-CJ-5.jpg";
+myImage.style.height = ("300px");
+
+let image2 = document.querySelector("#image-2");
+image2.style.height = ("300px");
+
+let no16 = document.querySelector("#item-16"); 
+no16.className = "item";
+no16.innerText = "Won't get fooled again.";
+
